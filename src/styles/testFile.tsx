c27b193452aafffcbb0e6a446c8602963f29474e
rmakulik <kulikr.js@mail.ru>
2024-11-13 23:09:21 +0300

    created new fork

---
+++ src/styles/testFile.tsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class testFile extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+.propTypes = {
+
+}
+
+export default 
