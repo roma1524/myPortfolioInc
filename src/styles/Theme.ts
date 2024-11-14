48e374312ba0873ac5e05c3475a9b604cbf1759b
rmakulik <kulikr.js@mail.ru>
2024-11-14 22:26:15 +0300

    added font, css rules, theme and container

---
+++ src/styles/Theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        primaryBg: 'rgb(4, 31, 49)',
+        secondaryBg: '#fffff0',
+        accent: '#ffff00',
+
+        font: 'rgb(189, 235, 234)',
+        fontError: 'rgb(192, 83, 83)'
+    }
+}
